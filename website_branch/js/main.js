4cdd3a8d5c44c9e2bd14af90726debcc88507be0
PhutureX <christoph.reich@gmx.at>
2016-10-30 18:17:44 +0100

    transforming header added

--- website_branch/js/main.js
+++ website_branch/js/main.js
@@ -1,1 +1,13 @@
-// TODO: everything Kappa
+//Sticky/Transforming Header Navigation
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){
+    $('header nav').addClass("transform-bg");
+    $('header nav img').addClass("showimg");
+    $('header nav ul li a').addClass("transform-menu");
+  }
+  else{
+    $('header nav').removeClass("transform-bg");
+    $('header nav img').removeClass("showimg");
+    $('header nav ul li a').removeClass("transform-menu");
+  }
+});
